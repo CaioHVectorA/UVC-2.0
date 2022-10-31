1758d1aea3a2391531caef4df22eabc5d30d5ee7
CaioHVectorA <caihebatista@gmail.com>
2022-10-31 16:40:10 -0300

    caixas automaticas v1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <header style={{display: 'flex',justifyContent:'space-between',width:'100%',padding:'0px',backgroundColor:'#B14060',marginBottom: '200px',paddingTop: '10px',paddingBottom: '10px'}}>
+        <header style={{display: 'flex',justifyContent:'space-between',width:'100%',padding:'0px',backgroundColor:'rgb(203 69 149)',marginBottom: '200px',paddingTop: '10px',paddingBottom: '10px'}}>
             <h2 style={{margin: '0px',marginLeft: '3%'}}>UVC</h2>
             <ul style={{marginRight: '3%',display: 'flex',gap: '16px'}}>
                <a href="#" style={{textDecoration: 'none'}}><li style={{fontSize: '24px',color: 'white'}}>Todos os Contos</li> </a>
